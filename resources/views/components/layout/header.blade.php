db740f9ec0e2ce10846a2f6890449491d5f7428c
Matthew Page <46349796+MatthewPageUK@users.noreply.github.com>
2023-02-25 22:21:13 +0000

    Basic layouts

---
+++ resources/views/components/layout/header.blade.php
@@ -0,0 +1,76 @@
+{{-- Main web site header component --}}
+<header
+    class="sticky top-0 flex space-x-8 items-center px-8 py-6 pt-10 bg-zinc-800 text-white border-b-2 border-amber-400 shadow-lg"
+>
+    <div>
+        <a href="/" class="border w-32 h-16 block">SITE LOGO</a>
+    </div>
+    <div class="flex space-x-2">
+        {{-- Skills --}}
+        <x-layout.header-button href="#" icon="construction">
+            {{ __('Skills') }}
+        </x-layout.header-button>
+
+        {{-- Experience --}}
+        <x-layout.header-button href="#" icon="public">
+            {{ __('Experience') }}
+        </x-layout.header-button>
+
+        {{-- Projects --}}
+        <x-layout.header-button href="#" icon="rocket_launch" :active="true">
+            {{ __('Projects') }}
+        </x-layout.header-button>
+
+        {{-- Demos --}}
+        <x-layout.header-button href="#" icon="smart_toy">
+            {{ __('Demos') }}
+        </x-layout.header-button>
+
+        {{-- Contact --}}
+        <x-layout.header-button href="#" icon="connect_without_contact">
+            {{ __('Contact') }}
+        </x-layout.header-button>
+    </div>
+    <div class="flex space-x-2 flex-grow justify-end">
+        {{-- Github Icon --}}
+        <x-layout.header-icon href="#" title="Github">
+            <x-icons.github class="w-8 h-8 text-white fill-white hover:fill-amber-400"/>
+        </x-layout.header-icon>
+
+        {{-- LinkedIn Icon --}}
+        <x-layout.header-icon href="#" title="LinkedIn">
+            <x-icons.linkedin class="w-8 h-8 text-white fill-white hover:fill-amber-400"/>
+        </x-layout.header-icon>
+
+        {{-- Youtube Icon --}}
+        <x-layout.header-icon href="#" title="Youtube">
+            <x-icons.youtube class="w-8 h-8 text-white fill-white hover:fill-amber-400"/>
+        </x-layout.header-icon>
+    </div>
+
+    {{-- Authentication --}}
+    @if (Route::has('login'))
+        <div class="flex items-center sm:fixed sm:top-0 sm:right-0 px-8 py-2 text-right text-sm">
+            @auth
+                {{-- Logged in user dashboard --}}
+                <a href="{{ url('/dashboard') }}" class="flex items-center text-white hover:text-amber-400 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
+                    Dashboard
+                    <span class="material-icons-outlined text-sm ml-1">account_circle</span>
+                </a>
+            @else
+                {{-- Log in --}}
+                <a href="{{ route('login') }}" class="flex items-center text-white hover:text-amber-400 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
+                    Log in
+                    <span class="material-icons-outlined text-sm ml-1">login</span>
+                </a>
+                @if (Route::has('register'))
+                    {{-- Register --}}
+                    <a href="{{ route('register') }}" class="flex items-center ml-4 text-white hover:text-amber-400 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
+                        Register
+                        <span class="material-icons-outlined text-sm ml-1">group_add</span>
+                    </a>
+                @endif
+            @endauth
+        </div>
+    @endif
+</header>
---
+++ resources/views/components/layout/masthead.blade.php
@@ -0,0 +1,6 @@
+<section
+    class="min-h-[256px] bg-black text-white"
+>
+    <iframe src="https://mjp.co/js/astar/demo-realtime.html" class="w-full min-h-[256px]"></iframe>
+</section>
+{{-- https://mjp.co/js/boids/masthead.html --}}
