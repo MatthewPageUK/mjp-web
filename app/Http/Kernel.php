2a8e195a62ead5eb2e3ce24c29da01f8dd90775f
Matthew Page <46349796+MatthewPageUK@users.noreply.github.com>
2023-09-09 15:45:11 +0100

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -46,6 +46,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\Authenticate::class,
+            \App\Http\Middleware\IsAdmin::class,
         ],
 
         'api' => [
