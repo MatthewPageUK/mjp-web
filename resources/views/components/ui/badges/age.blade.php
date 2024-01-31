0969004af0fd6bf7968d1d4ccd5951d5f40f76e0
Matthew Page <46349796+MatthewPageUK@users.noreply.github.com>
2024-01-31 23:00:13 +0000

    Update age.blade.php

--- resources/views/components/ui/badges/age.blade.php
+++ resources/views/components/ui/badges/age.blade.php
@@ -5,7 +5,7 @@
 <span {{ $attributes->merge(['class' => 'block text-xs text-primary-400 flex items-center content-end gap-1']) }}>
     <x-icons.material class="text-sm">calendar_month</x-icons.material>
     @if (! is_string($date))
-        {{ $date->diffForHumans() }}
+        {{ $date?->diffForHumans() }}
     @else
         {{ $date }}
     @endif
